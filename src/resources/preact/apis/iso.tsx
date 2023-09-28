94f6491a719bd2a62584a867e958b780429cbafa
Jason Caldwell <jaswrks@o5p.me>
2023-09-28 07:56:09 -0400

    Project update. [p][robotic]

--- src/resources/preact/apis/iso.tsx
+++ src/resources/preact/apis/iso.tsx
@@ -4,14 +4,13 @@
 
 import '../../init-env.ts';
 
-import { $http, $obj, $preact, type $type } from '@clevercanyon/utilities';
+import { $http, $preact, type $type } from '@clevercanyon/utilities';
 import { $cfp } from '../../../index.ts';
 
 /**
  * Defines types.
  */
 export type HandleSPACatchAllRouteOptions = Omit<$preact.iso.PrerenderSPAOptions, 'request'>;
-export type HydrativelyRenderSPAOptions = $preact.iso.HydrativelyRenderSPAOptions;
 
 /**
  * Handles an SPA's catch-all function route.
@@ -30,20 +29,9 @@ export const handleSPACatchAllRoute = async (feData: $cfp.FetchEventData, opts: 
     if (['HEAD', 'GET'].includes(request.method)) {
         const { httpState, docType, html } = await $preact.iso.prerenderSPA({ ...opts, request });
 
-        config.status = 200; // Potentially overridden by SPA.
+        config.status = httpState.status || 200;
         config.headers = { 'content-type': 'text/html; charset=utf-8' };
         config.body = docType + html; // HTML markup; including doctype.
-
-        config = $obj.mergeDeep(config, httpState) as unknown as Required<$http.ResponseConfig>;
     }
     return $http.prepareResponse(request, config) as $type.cf.Response;
 };
-
-/**
- * Hydrates SPA component on client-side.
- *
- * @param opts {@see HydrativelyRenderSPAOptions} for details.
- *
- * @note Client-side use only.
- */
-export const hydrativelyRenderSPA = $preact.iso.hydrativelyRenderSPA;
