8e69a03cf4031fa5f513aef86f669aa94e523ce6
Jason Caldwell <jaswrks@o5p.me>
2024-03-12 04:47:57 -0400

    Project update. [p][robotic]

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,3 @@ import '#@initialize.ts';
 export * as $build from '#build.ts';
 export * as $cfp from '#cfp.ts';
 export * as $preact from '#preact.ts';
-
-// Deprecated 2024-03-12.
-export * as $http from '#build.ts';
